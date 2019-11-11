3cd5d5c39e27b291637e4d8b0c07106354259eb6
Savio <dominicsaviovu@gmail.com>
2019-11-11 10:08:59 -0700

    minor changes to s to the name and what not

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Nav from "./components/Nav";
+import Player from "./components/Player";
+import Upload from "./components/Upload";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <Route exact path="/" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/main" component={Nav} />
+          <Route exact path="/upload" component={Upload} />
+          <Route exact path="/main" component={Player} />
+        </div> 
+      </Router>
+    );
+  }
 }
 
 export default App;
